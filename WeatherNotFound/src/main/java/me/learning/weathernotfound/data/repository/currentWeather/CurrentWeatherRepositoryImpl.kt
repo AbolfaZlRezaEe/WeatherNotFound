49040299690815e59336a77e67eeb7c538549b58
ABoFaZl ReZaEi <Abolfazlrezaei.of@gmail.com>
2023-06-06 14:53:42 +0330

    implement currentWeatherRepository

---
+++ WeatherNotFound/src/main/java/me/learning/weathernotfound/data/repository/currentWeather/CurrentWeatherRepositoryImpl.kt
@@ -0,0 +1,198 @@
+package me.learning.weathernotfound.data.repository.currentWeather
+
+import com.google.gson.Gson
+import com.google.gson.JsonSyntaxException
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import me.learning.weathernotfound.data.local.dao.CurrentWeatherDao
+import me.learning.weathernotfound.data.network.providers.RequestProvider
+import me.learning.weathernotfound.data.network.providers.UrlProvider
+import me.learning.weathernotfound.data.repository.Failure
+import me.learning.weathernotfound.data.repository.Response
+import me.learning.weathernotfound.data.repository.ResponseType
+import me.learning.weathernotfound.data.repository.Success
+import me.learning.weathernotfound.data.repository.WeatherNotFoundError
+import me.learning.weathernotfound.data.repository.WeatherNotFoundResponse
+import me.learning.weathernotfound.domain.currentWeather.Converters
+import me.learning.weathernotfound.domain.currentWeather.databaseModels.CurrentWeatherEntity
+import me.learning.weathernotfound.domain.currentWeather.networkModels.CurrentWeatherResponseModel
+import me.learning.weathernotfound.domain.currentWeather.presentationModels.CurrentWeatherModel
+import me.learning.weathernotfound.utils.Utilities.halfDayPassed
+import okhttp3.OkHttpClient
+
+internal class CurrentWeatherRepositoryImpl(
+    private val currentWeatherDao: CurrentWeatherDao,
+    private val okHttpClient: OkHttpClient,
+    private val gsonConverter: Gson,
+) : CurrentWeatherRepository {
+
+    private lateinit var currentWeatherInformationJob: Job
+    private lateinit var removeCacheInformationJob: Job
+
+    override fun getCurrentWeatherInformation(
+        latitude: Double,
+        longitude: Double,
+        resultInvoker: (Response<WeatherNotFoundResponse<CurrentWeatherModel>, WeatherNotFoundError>) -> Unit
+    ) {
+        currentWeatherInformationJob = CoroutineScope(Dispatchers.IO).launch {
+            val cacheResponse = currentWeatherDao.getCurrentWeatherEntityByCoordinates(
+                latitude = latitude,
+                longitude = longitude
+            )
+
+            if (cacheResponse != null) {
+                if (cacheResponse.updatedAt.halfDayPassed()) {
+                    // Cache information is no longer valid, should be updated with network request!
+                    startNetworkRequest(
+                        latitude = latitude,
+                        longitude = longitude,
+                        responseCallback = resultInvoker,
+                        responseReceivedCallback = { responseModel ->
+                            cacheResponseModelIntoDatabase(
+                                lastCurrentWeather = cacheResponse,
+                                currentWeatherResponseModel = responseModel
+                            )
+                        }
+                    )
+                } else {
+                    val weatherStatusList =
+                        currentWeatherDao.getWeatherStatusesByCurrentWeatherId(
+                            currentWeatherId = cacheResponse.id!!
+                        )
+                    resultInvoker.invoke(
+                        Success(
+                            WeatherNotFoundResponse(
+                                responseType = ResponseType.CACHE,
+                                responseModel = Converters.currentWeatherEntityToCurrentWeatherModel(
+                                    currentWeather = cacheResponse,
+                                    weatherStatuses = weatherStatusList
+                                )
+                            )
+                        )
+                    )
+
+                }
+            } else {
+                startNetworkRequest(
+                    latitude = latitude,
+                    longitude = longitude,
+                    responseCallback = resultInvoker,
+                    responseReceivedCallback = { responseModel ->
+                        cacheResponseModelIntoDatabase(
+                            lastCurrentWeather = null,
+                            currentWeatherResponseModel = responseModel
+                        )
+                    }
+                )
+            }
+        }
+    }
+
+    override fun removeCacheInformationOlderThan(timeStamp: Long) {
+        removeCacheInformationJob = CoroutineScope(Dispatchers.IO).launch {
+            currentWeatherDao.deleteCurrentWeatherEntitiesOlderThan(timeStamp)
+        }
+    }
+
+    override fun dispose() {
+        if (this::currentWeatherInformationJob.isInitialized
+            && !currentWeatherInformationJob.isCompleted
+            && !currentWeatherInformationJob.isCancelled
+        ) {
+            currentWeatherInformationJob.cancel()
+        }
+
+        if (this::removeCacheInformationJob.isInitialized
+            && !removeCacheInformationJob.isCompleted
+            && !removeCacheInformationJob.isCancelled
+        ) {
+            removeCacheInformationJob.cancel()
+        }
+    }
+
+    private suspend fun startNetworkRequest(
+        latitude: Double,
+        longitude: Double,
+        responseCallback: (Response<WeatherNotFoundResponse<CurrentWeatherModel>, WeatherNotFoundError>) -> Unit,
+        responseReceivedCallback: suspend (currentWeatherResponseModel: CurrentWeatherResponseModel) -> Unit
+    ) {
+        val request = RequestProvider.provideCurrentWeatherRequest(
+            url = UrlProvider.CURRENT_WEATHER_URL,
+            latitude = latitude,
+            longitude = longitude
+        )
+            ?: throw IllegalStateException("Request is null! Check currentWeatherInformation request!")
+
+        try {
+            val response = okHttpClient.newCall(request).execute()
+            if (response.isSuccessful && response.body != null) {
+                val responseModel: CurrentWeatherResponseModel?
+
+                try {
+                    responseModel = gsonConverter.fromJson(
+                        response.body!!.toString(),
+                        CurrentWeatherResponseModel::class.java
+                    )
+                } catch (jsonSyntaxException: JsonSyntaxException) {
+                    responseCallback.invoke(
+                        Failure(
+                            WeatherNotFoundError(
+                                exception = jsonSyntaxException,
+                                internalErrorMessage = "Failed to parse response into CurrentWeatherModel!"
+                            )
+                        )
+                    )
+                    return
+                }
+
+                responseCallback.invoke(
+                    Success(
+                        WeatherNotFoundResponse(
+                            httpResponseCode = response.code,
+                            responseType = ResponseType.NETWORK,
+                            responseModel = Converters.currentWeatherResponseModelToCurrentWeatherModel(
+                                responseModel
+                            )
+                        )
+                    )
+                )
+
+                // Notify callback to save the response into database
+                responseReceivedCallback.invoke(responseModel)
+            } else {
+                responseCallback.invoke(
+                    Failure(
+                        WeatherNotFoundError(
+                            httpResponseCode = response.code,
+                            httpResponseMessage = response.message,
+                        )
+                    )
+                )
+            }
+        } catch (exception: Exception) {
+            responseCallback.invoke(
+                Failure(WeatherNotFoundError(exception = exception))
+            )
+        }
+    }
+
+    private suspend fun cacheResponseModelIntoDatabase(
+        lastCurrentWeather: CurrentWeatherEntity?,
+        currentWeatherResponseModel: CurrentWeatherResponseModel,
+    ) {
+        val finalEntityModel =
+            Converters.currentWeatherResponseToCurrentWeatherEntity(
+                response = currentWeatherResponseModel,
+                entityId = lastCurrentWeather?.id
+            )
+        if (lastCurrentWeather == null) {
+            currentWeatherDao.insertCurrentWeatherEntity(finalEntityModel)
+        } else {
+            currentWeatherDao.updateCurrentWeatherEntity(
+                finalEntityModel
+            )
+        }
+    }
+}
